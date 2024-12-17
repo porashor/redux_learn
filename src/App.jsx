5a766e1cd8fccf4ac87f0d624f1ac00b3daa256b
Parashar Das <porashorbabu@gmail.com>
2024-12-17 23:28:33 +0600

    redux learning start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './Pages/Home'
+
+const App = () => {
+  return (
+    <div className='text-4xl text-green-200'>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
